596057b006fefef8f478096bd5311140b5ee865e
agus@mumuki.org <agus@mumuki.org>
2020-09-25 16:05:28 +0000

    Mumuki Export on 2020-09-25 16:05:28 +0000

--- 00003_Un programa con objetos/extra.rb
+++ 00003_Un programa con objetos/extra.rb
@@ -1,45 +1,13 @@
 module Pepita
   @energia = 100
-  @ciudad = Obera
-  
-  def self.energia=(una_energia) 
-    @energia = una_energia
-  end
-  
-  def self.energia
-    @energia 
-  end
-  
-  def self.ciudad
-    @ciudad 
-  end
-  
-  def self.cantar!
-    'pri pri pri'
-  end
   
   def self.comer_lombriz!
     @energia += 20
     return
   end
   
-  def self.comer_alpiste!(una_energia)
-    @energia += una_energia * 15
-    return
-  end
-  
   def self.volar_en_circulos!
     @energia -= 10
     return
-  end
-  
-  def self.volar_hacia!(una_ciudad)
-    @energia -= self.distancia(una_ciudad) * 3
-    @ciudad = una_ciudad
-    return
-  end
-  
-  def self.distancia(una_ciudad) 
-    (@ciudad.kilometro - una_ciudad.kilometro).abs
   end
 end
